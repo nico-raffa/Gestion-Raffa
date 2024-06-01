c8d7ce56087978c0ba5935bc65c88446ce5914d1
NicolasRaffa <djnikorc@gmail.com>
2024-06-01 12:06:00 -0300

    Config webhook

--- src/server.js
+++ src/server.js
@@ -38,3 +38,17 @@ app.get('*', (req, res) => {
         descripcion: `Ruta ${req.path} no implementada`
     })
 })
+
+app.post('/webhook', (req, res) => {
+    if (req.body.ref === 'refs/heads/master') {
+      exec('sh /home/server1/scripts/deploy.sh', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return res.sendStatus(500);
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+    }
+    res.sendStatus(200);
+  });
